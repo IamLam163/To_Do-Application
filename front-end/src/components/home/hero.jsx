21497c980f8b3d8f340013833ba2c5ffcdf9db92
IamLam163 <olamidealiyu@gmail.com>
2023-08-29 07:43:15 +0100

    fix scroll bug

--- front-end/src/components/home/hero.jsx
+++ front-end/src/components/home/hero.jsx
@@ -16,7 +16,6 @@ const useStyles = createStyles((theme) => ({
       "url(https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)",
     backgroundSize: "cover",
     backgroundPosition: "center",
-    height: "100vh",
   },
 
   container: {
